dfe9c326259e6667ef042dd9a034343bd0a42b1b
Ricardo Fontana <ricardo.fontana@catolicasc.org.br>
2024-10-09 22:20:34 -0300

    Until 99

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 function ext() {
     var numberInput = document.getElementById("number").value;
 
-    const units = ["zero", "um", "dois", "três", "quatro", "cinco"];
+    const units = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quartorze", "quinze", "dezesseis", "dezesete", "dezoito", "dezenove"];
+
+    const dozens = ["vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
 
     var number = parseInt(numberInput);
 
+    var desc = "";
+
+    if (number > 19) {
+        var dozen = parseInt(number/10);
+        desc += dozens[dozen-2];
+        var unity = number % 10;
+
+        if (unity > 0) {
+            desc += " e ";
+            desc += units[unity];
+        }
+    } else {
+        desc = units[number];
+    }
+
     var label = document.getElementById("ext")
-    label.innerHTML = units[number];
+    label.innerHTML = desc;
 
     console.log(number);
 }
